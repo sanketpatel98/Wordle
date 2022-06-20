e1a518ac0034eefe3d0ff613327b20ee295208ae
Sanket Patel <patelsanketr98@gmail.com>
2022-06-20 15:00:44 -0400

    UI Completed

--- wordle/ViewController.swift
+++ wordle/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextHorizontalStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
